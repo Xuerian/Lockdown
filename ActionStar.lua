14e17f0fac488f1274275841456dba6ffb508c38
Xuerian <sky.shell@gmail.com>
2014-05-18 08:58:53 -0400

    Convert from movement detection to on-unless-paused (By key or open UIs) - Crawl Apollo for escapable windows - Catch Abilities window that has weird properties - Include specified unescapable frames - Keybind to disable, handle esc properly

--- ActionStar.lua
+++ ActionStar.lua
@@ -1,9 +1,14 @@
-
--- Rights reserved, etc. Please do not host where not officially posted, distribution to friends is fine.
 -- Carbine, full rights if you want to use any of it.
+
+-- Add windows that don't close on escape here
+local tHardcodedForms = {
+	"RoverForm",
+	"GeminiConsoleWindow"
+}
 
 local ActionStar = {}
 
+-- Debug
 local function print(...)
 	local out = {...}
 	for i,v in ipairs(out) do
@@ -12,7 +17,10 @@ local function print(...)
 	Print(table.concat(out, ", "))
 end
 
+-- Startup
 function ActionStar:Init()
+	self.bActiveIntent = true
+	-- self.bIntentPaused = false
 	Apollo.RegisterAddon(self)
 end
 
@@ -27,47 +35,132 @@ function ActionStar:OnLoad()
 	-- Targeting
 	Apollo.RegisterEventHandler("MouseOverUnitChanged", "EventHandler_MouseOverUnitChanged", self)
 
-	-- Movement detection
-	Apollo.CreateTimer("ActionStar_Pulse", 0.2, true)
-	Apollo.RegisterTimerHandler("ActionStar_Pulse", "TimerHandler_Pulse", self)
+	-- Crawl for frames to hook
+	Apollo.CreateTimer("ActionStar_FrameCrawl", 5.0, false)
+	Apollo.RegisterTimerHandler("ActionStar_FrameCrawl", "TimerHandler_FrameCrawl", self)
+
+	-- Poll frames for visibility.
+	-- I'd much rather use hooks or callbacks or events, but I can't hook, I can't find the right callbacks/they don't work, and the events aren't consistant or listed. 
+	-- You made me do this, Carbine.
+	Apollo.CreateTimer("ActionStar_FramePollPulse", 0.5, true)
+	Apollo.RegisterTimerHandler("ActionStar_FramePollPulse", "TimerHandler_FramePollPulse", self)
+
+	-- Keybinds
+	Apollo.RegisterEventHandler("SystemKeyDown", "EventHandler_SystemKeyDown", self)
+
+	-- Carbine, plz.
+	Apollo.RegisterEventHandler("AbilityWindowHasBeenToggled", "EventHandler_AbilityWindowDecidedToShowUp", self)
 end
 
--- Targeting
-function ActionStar:EventHandler_MouseOverUnitChanged(unit)
-	if GameLib.IsMouseLockOn() then
-		GameLib.SetTargetUnit(unit)
+-- Disover frames we should pause for
+local tPauseWindows = {}
+local function RegisterWindow(wnd, bIsRandomMustRefresh)
+	if wnd then
+		local sName = wnd:GetName()
+		for k,v in pairs(tPauseWindows) do
+			if v == sName then
+				tPauseWindows[sName] = nil
+				break
+			end
+		end
+		tPauseWindows[wnd] = wnd:GetName()
 	end
 end
 
--- Movement detection
-local fMathAbs, iTicksTillUnlock, tPositionOld = math.abs, 0
-function ActionStar:TimerHandler_Pulse()
-	local unit = GameLib.GetPlayerUnit()
-	if unit and unit:IsValid() then -- Is this necessary? Will player ever be invalid?
-		local new, old = unit:GetPosition(), tPositionOld or unit:GetPosition()
-		tPositionOld = new
+function ActionStar:TimerHandler_FrameCrawl()
+	for _,strata in ipairs(Apollo.GetStrata()) do
+		for _,wnd in ipairs(Apollo.GetWindowsInStratum(strata)) do
+			if wnd:IsStyleOn("Escapable") and not wnd:IsStyleOn("CloseOnExternalClick") then
+				-- print(wnd:GetName())
+				RegisterWindow(wnd)
+			end
+		end
+	end
+	-- Hardcoded names, Carbine and authors, plz.
+	for _,sName in ipairs(tHardcodedForms) do
+		local wnd = Apollo.FindWindowByName(sName)
+		if wnd then
+			RegisterWindow(wnd)
+		end
+	end
+	self:SetActionMode(true)
+end
 
-		if fMathAbs(old.x-new.x) > 0.1 or fMathAbs(old.y-new.y) > 0.1 or fMathAbs(old.z-new.z) > 0.1 then
-			if not GameLib.IsMouseLockOn() then
-				self:SetActionMode(true)
-				iTicksTillUnlock = 5
-			end
-		elseif GameLib.IsMouseLockOn() then
-			-- Don't unlock immediately
-			if iTicksTillUnlock > 0 then
-				iTicksTillUnlock = iTicksTillUnlock - 1
-			else
-				self:SetActionMode(false)
-			end
+-- Poll unlocking frames
+-- TODO: Allow resuming with open windows. 
+local bWindowUnlock = false
+function ActionStar:TimerHandler_FramePollPulse()
+	bWindowUnlock = false
+	-- Poll windows
+	for wnd in pairs(tPauseWindows) do
+		if wnd:IsShown() then
+			bWindowUnlock = true
+		end
+	end
+	-- Update state 
+	if bWindowUnlock then
+		self:SuspendActionMode()
+	else
+		if self.bActiveIntent and not GameLib.IsMouseLockOn() then
+			self:SetActionMode(true)
 		end
 	end
 end
 
-function ActionStar:SetActionMode(bState)
-	GameLib.SetMouseLock(bState)
-	self.wndReticle:Show(bState)
+function ActionStar:EventHandler_AbilityWindowDecidedToShowUp()
+	local wnd = Apollo.FindWindowByName("AbilitiesBuilderForm")
+	if wnd then
+		RegisterWindow(wnd)
+	end
 end
 
+-- Keys
+function ActionStar:EventHandler_SystemKeyDown(iKey)
+	-- Open options on Escape
+	if iKey == 27 and self.bActiveIntent then
+		if GameLib.GetTargetUnit() then
+			GameLib.SetTargetUnit()
+			self:SetActionMode(true)
+		else
+			self:SuspendActionMode()
+		end
+	end
+
+	-- Toggle mode
+	if iKey == 67 and Apollo.IsShiftKeyDown()then
+		self:SetActionMode(not self.bActiveIntent)
+	end
+end
+
+-- Targeting
+function ActionStar:EventHandler_MouseOverUnitChanged(unit)
+	if unit and GameLib.IsMouseLockOn() then
+		GameLib.SetTargetUnit(unit)
+	end
+end
+
+-- Action mode toggle
+function ActionStar:SetActionMode(bState)
+	self.bActiveIntent = bState
+	-- self.bIntentPaused = false
+	if GameLib.SetMouseLock() ~= bState then
+		GameLib.SetMouseLock(bState)
+	end
+	self.wndReticle:Show(bState)
+	-- TODO: Sounds
+end
+
+-- Suspend without disabling
+function ActionStar:SuspendActionMode()
+	-- self.bIntentPaused = true
+	if GameLib.IsMouseLockOn() then
+		GameLib.SetMouseLock(false)
+		self.wndReticle:Show(false)
+	end
+	-- TODO: Indicate active-but-suspended status
+end
+
+-- Adjust reticle
 function ActionStar:Reticle_UpdatePosition()
 	local nRetW = self.wndReticle:FindChild("ActionStar_ReticleForm"):GetWidth()
 	local nRetH = self.wndReticle:FindChild("ActionStar_ReticleForm"):GetHeight() 
