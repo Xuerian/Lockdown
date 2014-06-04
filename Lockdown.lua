fa17b4da9367a3c4972d89f1fb7cd31a8b43304b
Xuerian <sky.shell@gmail.com>
2014-06-04 11:56:34 -0400

    Slow down modifier timer

--- Lockdown.lua
+++ Lockdown.lua
@@ -177,7 +177,7 @@ self.timerRelock = ApolloTimer.Create(0.01, false, "TimerHandler_Relock", self)
 	self.timerFrameCrawl = ApolloTimer.Create(4.0, false, "TimerHandler_FrameCrawl", self)
 
 	-- Wait for windows to be created or re-created
-	self.timerDelayedFrameCatch = ApolloTimer.Create(0.05, true, "TimerHandler_DelayedFrameCatch", self)
+	self.timerDelayedFrameCatch = ApolloTimer.Create(0.1, true, "TimerHandler_DelayedFrameCatch", self)
 	self.timerDelayedFrameCatch:Stop()
 
 	-- Poll frames for visibility.
